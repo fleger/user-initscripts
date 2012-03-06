e501cbdcd7b0c37abb0d856ecb1fc0fe3db6616f
fleger <florian6.leger@laposte.net>
2012-03-06 14:06:12 +0100

    Fix examples installation path

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ readonly SRC_DIR="$(dirname "$0")"
 install -Dm755  "${SRC_DIR}/bin/user-rc.d" \
                 "${DEST_DIR}/usr/sbin/user-rc.d"
 install -Dm644  "${SRC_DIR}/examples/user-rc.conf" \
-                "${DEST_DIR}/usr/share/examples/user-rc.conf"
+                "${DEST_DIR}/usr/share/user-initscripts/examples/user-rc.conf"
 install -Dm644  "${SRC_DIR}/hooks/user-initscripts" \
                 "${DEST_DIR}/etc/rc.d/functions.d/user-initscripts"
 install -Dm644  "${SRC_DIR}/user-rc.d/functions" \
